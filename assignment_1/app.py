b28087c33feb55a7d565b1d9a749d445dc23fc88
555isfaiz <1244274837@qq.com>
2023-04-03 22:18:29 +0200

    first try on REST

--- assignment_1/app.py
+++ assignment_1/app.py
@@ -1,10 +1,51 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+import json
 
 app = Flask(__name__)
+mapping = {}
+counter = 0
 
-@app.route('/')
-def home():
-    pass
+@app.route('/<id>', methods = ["GET"])
+def get_by_id(id):
+    if id not in mapping:
+        return "Not a valid id", 404
+    
+    return json.dumps("Location: {0}".format(mapping[id])), 301
+
+@app.route('/<id>', methods = ["PUT"])
+def put_by_id(id):
+    return "What should I do here..."
+
+@app.route('/<id>', methods = ["DELETE"])
+def del_by_id(id):
+    if id not in mapping:
+        return "Not a valid id", 404
+    
+    mapping.pop(id)
+    return 204
+
+@app.route('/', methods = ["GET"])
+def get_all_keys():
+    return json.dumps(list(mapping.keys()))
+
+@app.route('/', methods = ["POST"])
+def post_url():
+    global counter
+    if "url" not in request.args:
+        return "Wrong args", 400
+    
+    url = request.args.get("url")
+
+    # validate the url...
+
+    id = counter
+    counter = id + 1
+    mapping[id] = url
+    return str(id), 201
+
+@app.route('/', methods = ["DELETE"])
+def delete():
+    return "Not Found", 404
 
 if __name__ == '__main__':
     app.run(
