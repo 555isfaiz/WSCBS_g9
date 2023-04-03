e565e234a2c9328298f22b1268f1bf422ddee347
555isfaiz <1244274837@qq.com>
2023-04-03 15:15:00 +0200

    prepare assignment 1

---
+++ assignment_1/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    pass
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port= 53333,
+        debug=True
+    )
