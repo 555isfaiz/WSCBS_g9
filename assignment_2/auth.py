ab5480ff68364e328f2b27d44401223d819b1f69
Harry-Fei <wangyufei99@sina.com>
2023-04-24 19:53:14 +0200

    fix secret key prob

--- assignment_2/auth.py
+++ assignment_2/auth.py
@@ -8,7 +8,7 @@ import sys
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Flask(__name__)
-# auth.config["SECRET_KEY"] = "123456"
+auth.config["SECRET_KEY"] = "123456"
 users = {}
 
 use_db = False
@@ -22,13 +22,14 @@ try:
     class User(db.Model):
         username = db.Column(db.String(256), primary_key=True)
         password = db.Column(db.String(256), nullable=False)
+        start_time = db.Column(db.Integer, nullable=False)
 
         def __int__(self, _username, _password):
             self.username = _username
             self.password = _password
 
 
-    auth.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root@localhost/Website"
+    auth.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root@localhost/User"
     db.init_app(auth)
     with auth.app_context():
         db.create_all()
@@ -48,12 +49,13 @@ def authenticate():
     try:
         header_b64, payload_b64, signature_b64 = token.split('.')
         payload = json.loads(base64.urlsafe_b64decode(payload_b64).decode())
-        username = payload["name"]
+        username = payload["sub"]
         exp = payload["exp"]
+        iat = payload["iat"]
         message = header_b64 + '.' + payload_b64
-        expected_signature = hmac.new(users[username].encode(), message.encode(), hashlib.sha256)
+        expected_signature = hmac.new(auth.config["SECRET_KEY"].encode(), message.encode(), hashlib.sha256)
         expected_signature_b64 = base64.urlsafe_b64encode(expected_signature.digest()).decode()
-        if time.time() < exp and hmac.compare_digest(signature_b64, expected_signature_b64):
+        if time.time() < exp and iat > users[username][1] and hmac.compare_digest(signature_b64, expected_signature_b64):
             return jsonify({"Username": username}), 201
         return jsonify({"Message": "forbidden"}), 403
     except:
@@ -63,12 +65,13 @@ def authenticate():
 def register():
     username = request.json.get("username")
     password = request.json.get("password")
+    cur_time = time.time()
 
     if username not in users:
         hash_password = generate_password_hash(password=password)
-        users[username] = hash_password
+        users[username] = (hash_password, cur_time)
         if use_db:
-            new_user = User(username=username, password=hash_password)
+            new_user = User(username=username, password=hash_password, start_time=cur_time)
             db.session.add(new_user)
             db.session.commit()
         return jsonify({"Message": "success"}), 201
@@ -80,13 +83,15 @@ def change_password():
     username = request.json.get("username")
     old_password = request.json.get("old_password")
     new_password = request.json.get("new_password")
+    cur_time = time.time()
 
-    if check_password_hash(users[username], old_password):
+    if check_password_hash(users[username][0], old_password):
         hash_password = generate_password_hash(password=new_password)
-        users[username] = hash_password
+        users[username] = (hash_password, cur_time)
         if use_db:
             user = User.query.get(username)
             user.password = hash_password
+            user.start_time = cur_time
             db.session.commit()
         return jsonify({"Message": "success"}), 200
     else:
@@ -97,7 +102,7 @@ def login():
     username = request.json.get("username")
     password = request.json.get("password")
 
-    if username in users and check_password_hash(users[username], password):
+    if username in users and check_password_hash(users[username][0], password):
         # Generate authentication token
         header = {
             "alg": "HS256",
@@ -109,12 +114,12 @@ def login():
             "aud": "url_shortener_application",
             "nbf": int(time.time()),
             "iat": int(time.time()),
-            "exp": int(time.time() + 300),
+            "exp": int(time.time() + 300)
         }
         header_b64 = base64.urlsafe_b64encode(json.dumps(header).encode()).decode()
         payload_b64 = base64.urlsafe_b64encode(json.dumps(payload).encode()).decode()
         message = header_b64 + '.' + payload_b64
-        signature = hmac.new(users[username].encode(), message.encode(), hashlib.sha256)
+        signature = hmac.new(auth.config["SECRET_KEY"].encode(), message.encode(), hashlib.sha256)
         signature_b64 = base64.urlsafe_b64encode(signature.digest()).decode()
         token = message + '.' + signature_b64
         return jsonify({"JWT": token}), 200
